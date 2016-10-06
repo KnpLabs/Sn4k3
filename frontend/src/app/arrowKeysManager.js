63c40c4bb79cf1472fef005ad5df56e7889641b4
aguidis <adrien.guidis@outlook.com>
2016-10-06 17:00:12 +0200

    const in class not possible

--- frontend/src/app/arrowKeysManager.js
+++ frontend/src/app/arrowKeysManager.js
@@ -6,21 +6,20 @@
  */
 class ArrowKeysManager {
 
-    const KEY_LEFT = 37;
-    const KEY_RIGHT = 39;
-
     constructor(CrossbarConnection) {
+        this.KEY_LEFT = 37;
+        this.KEY_RIGHT = 39;
         this.crossbarConnection = CrossbarConnection;
     }
-    
+
     init() {
         document.onkeyup = this.handleKeyUp.bind(this);
         document.onkeydown = this.handleKeyDown.bind(this);
     }
-    
+
     handleKeyUp(e) {
         switch (e.keyCode) {
-            case ArrowKeysManager.KEY_LEFT:
+            case this.KEY_LEFT:
 
                 // Publish on topic "action" to broadcast
                 // and tell to others client to draw the circles
@@ -29,41 +28,41 @@ class ArrowKeysManager {
                     direction: 'left',
                     pressed: false
                 });
-                
+
                 break;
 
-            case ArrowKeysManager.KEY_RIGHT:
-                
+            case this.KEY_RIGHT:
+
                 this.crossbarConnection.session.publish('action', {
                     username: window.username,
                     direction: 'right',
                     pressed: false
                 });
-                
+
                 break;
         }
     }
 
     handleKeyDown(e) {
         switch (e.keyCode) {
-            case ArrowKeysManager.KEY_LEFT:
+            case this.KEY_LEFT:
 
                 this.crossbarConnection.session.publish('action', {
                     username: window.username,
                     direction: 'right',
                     pressed: true
                 });
-                
+
                 break;
 
-            case ArrowKeysManager.KEY_RIGHT:
+            case this.KEY_RIGHT:
 
                 this.crossbarConnection.session.publish('action', {
                     username: window.username,
                     direction: 'right',
                     pressed: true
                 });
-                
+
                 break;
         }
     }
