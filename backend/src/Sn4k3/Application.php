214bc5688762518fbc2cbd35c4643a443a5a8a49
Alex Rock Ancelet <pierstoval@gmail.com>
2016-10-06 18:05:38 +0200

    Refactor constant names in Application

--- backend/src/Sn4k3/Application.php
+++ backend/src/Sn4k3/Application.php
@@ -3,24 +3,40 @@
 namespace Sn4k3;
 
 use React\EventLoop\Factory;
+use React\EventLoop\LoopInterface;
 use Sn4k3\Socket\WebSocket;
 use Thruway\ClientSession;
 
 class Application
 {
-    const ACTION = 'action';
-    const TICK = 'tick';
-    const JOIN = 'join';
+    const CROSSBAR_WEBSOCKET_PORT = 7777;
+    const CROSSBAR_WEBSOCKET_PATH = 'sn4k3';
 
+    const EVENT_INCOMING_ACTION = 'action';
+    const EVENT_INCOMING_JOIN = 'join';
+    const EVENT_OUTGOING_TICK = 'tick';
+
+    /**
+     * @var LoopInterface
+     */
     private $loop;
+    
+    /**
+     * @var Game
+     */
     private $game;
+    
+    /**
+     * @var WebSocket
+     */
     private $webSocket;
 
     public function __construct()
     {
         $this->loop = Factory::create();
         $this->game = new Game($this->loop);
-        $this->webSocket = new WebSocket(7777, 'sn4k3', $this->loop);
+        $this->webSocket = new WebSocket(self::CROSSBAR_WEBSOCKET_PORT, self::CROSSBAR_WEBSOCKET_PATH, $this->loop);
+
         $this->listenIncomingMessages();
         $this->broadcastTick();
         $this->listenForNewPlayers();
@@ -35,8 +51,8 @@ class Application
         $promise = $this->webSocket->promiseSession();
 
         $promise->then(function (ClientSession $session) {
-            $session->subscribe(self::ACTION, function ($_, $args) {
-                if (!isset($args->playerName) || !isset($args->direction)) {
+            $session->subscribe(self::EVENT_INCOMING_ACTION, function ($_, $args) {
+                if (!isset($args->playerName, $args->direction)) {
                     return;
                 }
 
@@ -52,7 +68,7 @@ class Application
     private function listenForNewPlayers()
     {
         $this->webSocket->promiseSession()->then(function (ClientSession $session) {
-            $session->subscribe(self::JOIN, function($_, $args) {
+            $session->subscribe(self::EVENT_INCOMING_JOIN, function($_, $args) {
                 if (!isset($args->playerName) || strlen($args->playerName) < 3) {
                     return;
                 }
@@ -67,7 +83,7 @@ class Application
         $this->game->on(Game::EVENT_TICK, function () {
             $this->webSocket->promiseSession()->then(function (ClientSession $session) {
                 $data = Serializer::serializeGame($this->game);
-                $session->publish(self::TICK, null, $data);
+                $session->publish(self::EVENT_OUTGOING_TICK, null, $data);
             });
         });
     }
