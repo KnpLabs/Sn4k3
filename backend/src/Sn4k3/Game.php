f1c2f03734d271d52239819679e27c7c93b8f52a
Alex Rock Ancelet <pierstoval@gmail.com>
2016-10-06 12:14:11 +0200

    Create the Game object

---
+++ backend/src/Sn4k3/Game.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Sn4k3;
+
+class Game
+{
+    /**
+     * In milliseconds, corresponds to the tick interval.
+     *
+     * @var int
+     */
+    public $tickInterval;
+
+    public function __construct()
+    {
+    }
+
+    public function run()
+    {
+    }
+
+    public function getPlayerByHash($hash)
+    {
+    }
+}
