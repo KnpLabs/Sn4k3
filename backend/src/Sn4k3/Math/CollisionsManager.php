1fafbb8985c4ea5865cf4a3ba9ed4b6787f07460
Alex Rock Ancelet <pierstoval@gmail.com>
2016-10-08 11:50:29 +0200

    Added rectangle collision calculation

--- backend/src/Sn4k3/Math/CollisionsManager.php
+++ backend/src/Sn4k3/Math/CollisionsManager.php
@@ -3,6 +3,7 @@
 namespace Sn4k3\Math;
 
 use Sn4k3\Geometry\Circle;
+use Sn4k3\Geometry\Rectangle;
 use Sn4k3\Model\CollisionableInterface;
 
 final class CollisionsManager
@@ -42,7 +43,14 @@ final class CollisionsManager
         $deltaY = $circleA->centerPoint->y - $circleB->centerPoint->y;
 
         // First naive guess.
-        if ($deltaX > 100 || $deltaY > 100) {
+        if ($deltaX > 75 || $deltaY > 75) {
+            return false;
+        }
+
+        // Square test (less performance consuming)
+        $rect1 = Rectangle::createFromCircle($circleA);
+        $rect2 = Rectangle::createFromCircle($circleB);
+        if (!static::testRectanglesCollision($rect1, $rect2)) {
             return false;
         }
 
@@ -51,5 +59,21 @@ final class CollisionsManager
         $radiusSum = $circleA->radius + $circleB->radius;
 
         return ($distance < $radiusSum);
+    }
+
+    /**
+     * @param Rectangle $rectangleA
+     * @param Rectangle $rectangleB
+     *
+     * @return bool
+     */
+    public static function testRectanglesCollision(Rectangle $rectangleA, Rectangle $rectangleB): bool
+    {
+        return
+            $rectangleA->x < $rectangleB->x + $rectangleB->width &&
+            $rectangleA->x + $rectangleA->width > $rectangleB->x &&
+            $rectangleA->y < $rectangleB->y + $rectangleB->height &&
+            $rectangleA->height + $rectangleA->y > $rectangleB->y
+        ;
     }
 }
